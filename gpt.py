0d964472b4f1a7fc3f6ca0946466ce1c4de87d72
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-09-01 14:33:57 +0700

    first

---
+++ gpt.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    # api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key='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'
+)
+
+response = client.responses.create(
+    model="gpt-4o",
+    instructions="You are a coding assistant that talks like a pirate.",
+    input="How do I check if a Python object is an instance of a class?",
+)
+
+print(response.output_text)
---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def handle_request():
+    name = request.args.get('name', default='')
+    chat_id = request.args.get('id', default='0')
+    item = request.args.get('item', default='')
+
+    return 'ok'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

24181d5a12ebef3be45db6f7bcd1bfcce94e001c
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-09-01 14:37:58 +0700

    first00

--- gpt.py
+++ gpt.py
@@ -4,7 +4,6 @@ from openai import OpenAI
 client = OpenAI(
     # This is the default and can be omitted
     # api_key=os.environ.get("OPENAI_API_KEY"),
-    # api_key='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'
 )
 
 response = client.responses.create(

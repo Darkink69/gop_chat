9f8d365a60e82ce2ba97e29ddb5c377b16850dff
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-09-11 09:28:46 +0700

    banana_0.21

--- banana.py
+++ banana.py
@@ -7,19 +7,20 @@ from io import BytesIO
 def get_banana():
     client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
-    # response = client.models.generate_content(
-    #     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-    # )
-    # print(response.text)
-    response = client.models.generate_images(
-        model='imagen-4.0-generate-001',
-        prompt='Robot holding a red skateboard',
-        config=types.GenerateImagesConfig(
-            number_of_images=1,
-        )
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents="Explain how AI works in a few words"
     )
-    for generated_image in response.generated_images:
-        generated_image.image.show()
-        return generated_image.image
+    print(response.text)
+    return response.text
+    # response = client.models.generate_images(
+    #     model='imagen-4.0-generate-001',
+    #     prompt='Robot holding a red skateboard',
+    #     config=types.GenerateImagesConfig(
+    #         number_of_images=1,
+    #     )
+    # )
+    # for generated_image in response.generated_images:
+    #     generated_image.image.show()
+    #     return generated_image.image
 
 

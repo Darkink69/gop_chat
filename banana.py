f9b32355d3bbee75d8b8f135e9a6f73367a4860c
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-09-14 13:24:14 +0700

    gop_0.31

--- banana.py
+++ banana.py
@@ -1,8 +1,6 @@
 import os
 from google import genai
 from google.genai import types
-from PIL import Image
-from io import BytesIO
 
 def get_banana(prompt):
     client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
